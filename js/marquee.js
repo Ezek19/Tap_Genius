e12a0851253f9678df8a2f494c3ab405d44e8649
Ezek <ezek79@yahoo.com>
2014-04-02 13:26:08 +0800

    t

---
+++ js/marquee.js
@@ -0,0 +1,16 @@
+//<![CDATA[
+marqueeInit({
+uniqueid: 'mycrawler',
+style: {
+'margin-top': '2px',
+'padding': '1px',
+'width': '945px',
+'height': '80px'
+},
+inc: 4, //speed - pixel increment for each iteration of this marquee's movement
+mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+moveatleast: 1,
+neutral: 150,
+savedirection: true
+});
+//]]> 
